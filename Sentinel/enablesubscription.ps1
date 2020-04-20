dc1cdd31b8158c663c3a70c1a115ab83157fe101
Jon Nordstrom <35916806+jonnords@users.noreply.github.com>
2020-04-20 11:04:36 +0200

    Create enablesubscription.ps1

---
+++ Sentinel/enablesubscription.ps1
@@ -0,0 +1,17 @@
+#Enable the Activity API Subscriptions
+
+$clientID = "YOUR CLIENT ID”
+$clientSecret = "YOUR CLIENT SECRET"
+$loginURL = "https://login.windows.net"
+$tenantdomain = "YOUR TENANT"
+$tenantGUID = "YOUR TENANT GUID"
+$resource = "https://manage.office.com"
+# Get an Oauth 2 access token based on client id, secret and tenant domain
+$body = @{grant_type="client_credentials";resource=$resource;client_id=$ClientID;client_secret=$ClientSecret}
+$oauth = Invoke-RestMethod -Method Post -Uri $loginURL/$tenantdomain/oauth2/token?api-version=1.0 -Body $body
+#Let's put the oauth token in the header
+$headerParams  = @{'Authorization'="$($oauth.token_type) $($oauth.access_token)"}
+#Start Subscriptions
+Invoke-RestMethod -Method Post -Headers $headerParams -Uri "https://manage.office.com/api/v1.0/$tenantGUID/activity/feed/subscriptions/start?contentType=DLP.All"
+#List the active subscriptions
+Invoke-WebRequest -Headers $headerParams -Uri "https://manage.office.com/api/v1.0/$tenantGUID/activity/feed/subscriptions/list"
