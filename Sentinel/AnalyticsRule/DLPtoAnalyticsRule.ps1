589d8aa8c341ba73f28bbace23e42ed11348401e
Jon Nordstrom <35916806+jonnords@users.noreply.github.com>
2020-04-24 11:35:29 +0200

    Create DLPtoAnalyticsRule.ps1

---
+++ Sentinel/AnalyticsRule/DLPtoAnalyticsRule.ps1
@@ -0,0 +1,62 @@
+#Path to the template file
+$filepath = "c:\ps\dlp\"
+$dlpyamlfile = $filepath + "ruletemplate.yaml"
+
+#Sentinel variables
+$subscriptionID = "The SubscriptionID GUID"
+$workspacename = "The Sentinel Worksapce Name"
+
+$processedPolicies = @()
+$dlpyaml = Get-Content $dlpyamlfile
+
+#Use this portion if you want to automate using a Function
+#$expass = $env:expass
+#$exuser = $env:exuser
+#$password = ConvertTo-SecureString $expass -AsPlainText -Force
+#$credentials=New-Object -TypeName System.Management.Automation.PSCredential ($exuser, $password)
+# Connect-AzAccount -Credential $Credentials -Tenant "GUID" -servicePrincipal
+
+Connect-AzAccount 
+$credentials = Get-Credential
+
+
+#Connecting to SCC PowerShell
+if ($credentials) {
+$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri  https://ps.compliance.protection.outlook.com/powershell-liveid?BasicAuthToOAuthConversion=true -Credential $Credentials -Authentication Basic -AllowRedirection
+                  }
+
+if ($session) {Import-PSSession $session -CommandName Get-DlpComplianceRule  -AllowClobber -DisableNameChecking}
+
+#Retreiving the DLP Policies in place 
+$policies = Get-DlpComplianceRule
+
+# Looping through the policies and create Analytic Rules in Sentinel
+foreach ($policy in $policies) {
+
+if ($processedPolicies -notcontains $policy.ReportSeverityLevel,$policy.ParentPolicyName) 
+                                  {
+
+        #Updating with the severity and name of the Dlp Policy
+
+        $policyName = $policy.ParentPolicyName + "_" + $policy.ReportSeverityLevel
+        $updateyaml1 = $dlpyaml -replace "dlppolicy", $policyName
+        $finalyaml = $updateyaml1 -replace "UpdateSeverity", $policy.ReportSeverityLevel
+
+            # Sending the rule to temporary storage
+            $file = $filepath + $policy.ParentPolicyName + $policy.ReportSeverityLevel + ".yaml"
+            $finalyaml | Out-File $file
+
+             #Updating or adding new Rules to Sentinel
+             Get-Item $file  | Import-AzSentinelAlertRule -WorkspaceName $workspacename -SubscriptionID $subscriptionID
+            
+          #Keep track of already processed rules by placing in array for if sentence
+          $processedPolicies += $policy.ReportSeverityLevel,$policy.ParentPolicyName
+
+Clear-Variable updateyaml1
+Clear-Variable finalyaml
+
+                                }
+
+Remove-PSSession $session
+Disconnect-AzAccount  
+                          }
