efb4ac8b7620b2da9098829773dd6c32ccbe9a81
Jon Nordstrom <35916806+jonnords@users.noreply.github.com>
2021-10-15 20:36:06 +0200

    Add multi workspace code to main branch

--- Sentinel/StoreEvents.ps1
+++ Sentinel/StoreEvents.ps1
@@ -1,14 +1,31 @@
 # Input bindings are passed in via param block.
 param($QueueItem, $TriggerMetadata)
 
-# Replace with your Log Analytics Workspace ID
-$CustomerId = $env:workspaceId
+#Initiate Arrays used by the function
+$records = @()
+$exupload = @()
+$spoupload = @()
+$allWS = @()
 
-# Replace with your Log Analytics Primary Key
-$SharedKey = $env:workspaceKey
+$tolocal = @()
+
+#Workspaces, mapping of country code to workspace, update county code, key and ID reference for multiple workspaces
+#$maineu = @{"Countries" = "GB,ES,IT";"Workspacekey" = $env:workspaceKey; "Workspace" = $env:workspaceId}
+#$Germany = @{"Countries" = "DE";"Workspacekey" = $env:workspaceKeyEU; "Workspace" = $env:workspaceIdEU}
+#$LT = @{"Countries" = "LT";"Workspacekey" = $env:workspaceKeyEU; "Workspace" = $env:workspaceIdEU}
+
+#List of Workspaces, update based on workspaces added
+#$workspaces = @{"MainEU" = $maineu; "Germany" = $Germany; "LT" = $LT}
+
+#All Content Workspace, used for the central team
+$AllContent = @{"Countries" = "ALLContent";"Workspacekey" = $env:workspaceKey; "Workspace" = $env:workspaceId}
+
 
 # Specify the name of the record type that you'll be creating
 $LogType = $env:customLogName
+
+# Specify the table used for Endpoint
+$logTypeEndpoint = "endpointdlp"
 
 #SharePoint Site US
 $SPUS = $env:SPUS
@@ -48,13 +65,6 @@ function Test-Command {
 
 # You can use an optional field to specify the timestamp from the data. If the time field is not specified, Azure Monitor assumes the time is the message ingestion time
 $TimeStampField = (Get-Date)
-
-#Initiate Arrays used by the function
-$records = @()
-$exupload = @()
-$spoupload = @()
-$usWorkspace = @()
-$tolocal = @()
 
 # Create the function to create the authorization signature
 Function Build-Signature ($customerId, $sharedKey, $date, $contentLength, $method, $contentType, $resource)
@@ -148,9 +158,21 @@ $headerParamsG  = @{'Authorization'="$($oauthG.token_type) $($oauthG.access_toke
 
 
 Foreach ($user in $records) {
+#Capture detection entries that are too long for LA to store
+if (($user.PolicyDetails.rules.ConditionsMatched.SensitiveInformation.SensitiveInformationDetections.ResultsTruncated -eq "true") -or ($user.PolicyDetails.rules.ConditionsMatched.SensitiveInformation.SensitiveInformationDetections.Count -gt 150)) {
+$detectedrows = $user.PolicyDetails.rules.ConditionsMatched.SensitiveInformation.SensitiveInformationDetections.DetectedValues.count
+do      {
+    $increment = [math]::truncate($detectedrows/2 )
+    $detected = $user.PolicyDetails.rules.ConditionsMatched.SensitiveInformation.SensitiveInformationDetections.DetectedValues[0..$increment]
+    $detectedcount = $detected.value | Measure-object -Character
+    $detectedrows = [math]::truncate($increment*1.5)
+        } until ($detectedcount.Characters -le "10240" )
+            $user.PolicyDetails.rules.ConditionsMatched.SensitiveInformation.SensitiveInformationDetections.DetectedValues = $detected
+                                                                                                                                  }
+
 #Exchange and Teams upload data process
 $user.workload
-if (($user.workload -eq "Exchange") -or ($user.Workload -eq "MicrosoftTeams")) {
+if (($user.workload -eq "Exchange" -and $user.operation -ne "MipLabel") -or ($user.Workload -eq "MicrosoftTeams")) {
 
  #Determine if the email is from external or internal if from external associate with first recipient on the to line
     if (($env:domains).split(",") -Contains ($user.ExchangeMetaData.from.Split('@'))[1]) {$exuser = $user.ExchangeMetaData.from}
@@ -166,15 +188,15 @@ if (($user.workload -eq "Exchange") -or ($user.Workload -eq "MicrosoftTeams")) {
     if (($systemMail).split(",") -notcontains $exuser) {
         
         #Add the additional attributes needed to enrich the event stored in Log Analytics for Exchange
-        $queryString = "https://graph.microsoft.com/v1.0/users/" + $exuser + "?" + "$" + "select=usageLocation,Manager,department,state"       
-        $info = Invoke-RestMethod -Headers $headerParamsG -Uri $queryString -Method GET
+        $queryString = "https://graph.microsoft.com/v1.0/users/" + $exuser + "?" + "$" + "select=usageLocation,Manager,department,state"        
+        $info = Invoke-RestMethod -Headers $headerParamsG -Uri $queryString -Method GET -SkipHttpErrorCheck
 
         #Add usage location from GRAPH Call
         $user | Add-Member -MemberType NoteProperty -Name "usageLocation" -Value $info.usageLocation
          if ($info) {$user | Add-Member -MemberType NoteProperty -Name "department" -Value $info.department}
 
         $querymanager = "https://graph.microsoft.com/v1.0/users/" + $exuser + "/manager"
-        $manager = Invoke-RestMethod -Headers $headerParamsG -Uri $querymanager
+        $manager = Invoke-RestMethod -Headers $headerParamsG -Uri $querymanager -SkipHttpErrorCheck
         if ($manager) {$user | Add-Member -MemberType NoteProperty -Name "manager" -Value $manager.mail}
         
     if ($user.workload -eq "Exchange") {
@@ -191,6 +213,7 @@ if (($user.workload -eq "Exchange") -or ($user.Workload -eq "MicrosoftTeams")) {
                                                             }   
       Clear-Variable -name info                                                                                                         
                                                          }
+
 $exupload += $user 
                                     }
 
@@ -199,32 +222,71 @@ if (($user.Workload -eq "OneDrive") -or ($user.Workload -eq "SharePoint")) {
 
     #Add the additional attributes needed to enrich the event stored in Log Analytics for SharePoint
     $queryString = $user.SharePointMetaData.From + "?$" + "select=usageLocation,Manager,department,state"
-    $info = Invoke-RestMethod -Headers $headerParamsG -Uri "https://graph.microsoft.com/v1.0/users/$queryString" -Method GET
+    $info = Invoke-RestMethod -Headers $headerParamsG -Uri "https://graph.microsoft.com/v1.0/users/$queryString" -Method GET -SkipHttpErrorCheck
     $user | Add-Member -MemberType NoteProperty -Name "usageLocation" -Value $info.usageLocation
      if ($info) {$user | Add-Member -MemberType NoteProperty -Name "department" -Value $info.department}
 
-        $querymanager = "https://graph.microsoft.com/v1.0/users/" + $user.SharePointMetaData.From + "/manager"
-        $manager = Invoke-RestMethod -Headers $headerParamsG -Uri $querymanager
+        $querymanager = "https://graph.microsoft.com/v1.0/users/" + $user.SharePointMetaData.From + "/manager" 
+        $manager = Invoke-RestMethod -Headers $headerParamsG -Uri $querymanager -SkipHttpErrorCheck
         if ($manager) {$user | Add-Member -MemberType NoteProperty -Name "manager" -Value $manager.mail}
 
 $spoupload += $user
 Clear-Variable -name info
                                                                             }
+                            
+#EndpointDLP upload
+if (($user.Workload -eq "EndPoint") -and (($user.PolicyMatchInfo)))   {
+
+    #Add the additional attributes needed to enrich the event stored
+    $queryString = $user.UserKey + "?$" + "select=usageLocation,Manager,department,state"
+    $info = Invoke-RestMethod -Headers $headerParamsG -Uri "https://graph.microsoft.com/v1.0/users/$queryString" -Method GET -SkipHttpErrorCheck
+    $user | Add-Member -MemberType NoteProperty -Name "usageLocation" -Value $info.usageLocation
+     if ($info) {$user | Add-Member -MemberType NoteProperty -Name "department" -Value $info.department}
+     if ($user.objectId) {
+         $document = Split-Path $user.objectId -leaf
+         $user | Add-Member -MemberType NoteProperty -Name "DocumentName" -Value $document
+                         }
+
+$endpointupload += $user
+Clear-Variable -name info
+                                          }                            
                              }    
 
 
-#Determine which Sentinel Workspace to route the information, remember to define the variable for each workspace as an array.
-foreach ($entry in $exupload)   {
-        if ($entry.usageLocation -eq "US") { $usWorkspace += $entry  }
-        if ($entry.usageLocation -ne "US")   { $usWorkspace += $entry  } 
-                                }
-foreach ($entry in $spoupload)  {
-        if ($entry.usageLocation -eq "US") { $usWorkspace += $entry  }    
-        if ($entry.usageLocation -ne "US")   { $usWorkspace += $entry  } 
-                                }
 
-#Upload US Workspace, to add addtional workspaces add the WorkspaceID and Workspacekey and make a new post based on those parameters
-if ($usWorkspace) {
-$jsonus = $usWorkspace | convertTo-Json -depth 20
-Post-LogAnalyticsData -customerId $env:workspaceId -sharedKey $env:workspaceKey -body ([System.Text.Encoding]::UTF8.GetBytes($jsonus)) -logType $logType
-                  }
+#Determine which Sentinel Workspace to route the information,
+$uploadWS = @{}
+
+if ($workspace) 
+{
+foreach ($workspace in $workspaces.GetEnumerator()) {
+$uploadWS[$workspace.name] = @()
+
+#Exchange and Teams
+$uploadWS[$workspace.name] += $exupload | where-object  {$workspace.Value.Countries.split(",") -Contains $_.usageLocation}                                                          
+
+#SharePoint and OneDrive
+$uploadWS[$workspace.name] += $spoupload | where-object  {$workspace.Value.Countries.split(",") -Contains $_.usageLocation}       
+                                
+#EndPoint
+$uploadWS[$workspace.name] += $endpointupload | where-object  {$workspace.Value.Countries.split(",") -Contains $_.usageLocation}    
+
+                                                        }                  
+
+#Upload to Workspaces
+
+foreach ($workspace in $workspaces.GetEnumerator()) {
+$activeWS = $workspace.name
+if ($uploadWS.$activeWS) {
+$json = $uploadWS.$activeWS |convertTo-Json -depth 100
+Post-LogAnalyticsData -customerId $workspace.value.workspace -sharedKey $workspace.value.workspacekey -body ([System.Text.Encoding]::UTF8.GetBytes($json)) -logType $logType
+                        }
+
+                                                    }
+}                       
+#Uploading everything to a unified Workspace
+$allWS += $exupload
+$allWS += $spoupload
+$allWS += $endpointupload
+$alljson = $allWS |convertTo-Json -depth 100
+Post-LogAnalyticsData -customerId $AllContent.workspace -sharedKey $AllContent.workspacekey -body ([System.Text.Encoding]::UTF8.GetBytes($alljson)) -logType $logType
