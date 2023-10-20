d4cce568bf037407ca7f3ec4ae30efabc1825942
Alex Anders <alex@andersconnection.comm>
2023-10-20 12:30:51 -0600

    Updates

--- Sentinel_Deployment/maintenance/updateCode.ps1
+++ Sentinel_Deployment/maintenance/updateCode.ps1
@@ -1,3 +1,8 @@
+#Download latest Function App package.
 Invoke-WebRequest -Uri "https://github.com/anders-alex/O365-ActivityFeed-AzureFunction/raw/Sentinel_Deployment3/Sentinel_Deployment/functionPackage.zip" -OutFile "functionPackage.zip"
 
-Publish-AzWebapp -ResourceGroupName " " -Name " " -ArchivePath .\functionPackage.zip -Force -Restart
+#Select the subscription that contains the Function App to be updated.
+Set-AzContext -Subscription '[Subscription name]'
+
+#Update Function App with the new package.
+Publish-AzWebapp -ResourceGroupName "[Resource group name]" -Name "[Function App name]" -ArchivePath functionPackage.zip -Force
